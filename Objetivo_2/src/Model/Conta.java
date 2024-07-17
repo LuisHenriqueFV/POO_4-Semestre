31648db50af979c812af35af3ff7fafeac301c1b
Henrique <luishenriquefonsecavictoria.dev@gmail.com>
2024-07-17 20:51:38 -0300

    aprimorando modelo de Conta

--- Objetivo_2/src/Model/Conta.java
+++ Objetivo_2/src/Model/Conta.java
@@ -12,4 +12,25 @@ public abstract class Conta {
         System.out.println("Atualizando...");
     }
 
+    public Conta() {
+    }
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "\nConta{" +
+                "saldo=" + saldo +
+                '}';
+    }
 }
