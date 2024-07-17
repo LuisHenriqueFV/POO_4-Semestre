a6c15248e94c284897e57aa2079b685798edfd6c
Henrique <luishenriquefonsecavictoria.dev@gmail.com>
2024-07-16 23:02:00 -0300

    criação da classe, construtores, acessores

---
+++ src/Objetivo_1/Controller/AlunoController.java
@@ -0,0 +1,11 @@
+package Objetivo_1.Controller;
+
+import Objetivo_1.Model.Aluno;
+
+public class AlunoController {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+
+
+    }
+}
