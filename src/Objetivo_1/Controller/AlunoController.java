708b37425d233f7bedf0c48afed1ce2968a6d290
Henrique <luishenriquefonsecavictoria.dev@gmail.com>
2024-07-17 11:27:40 -0300

    Implementação da primeira parte do objetivo: criação e modificação de instâncias da classe Aluno

--- src/Objetivo_1/Controller/AlunoController.java
+++ src/Objetivo_1/Controller/AlunoController.java
@@ -4,19 +4,37 @@ import Objetivo_1.Model.Aluno;
 
 public class AlunoController {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
-        Aluno aluno2 = new Aluno();
-        Aluno aluno3 = new Aluno(1, 324345642, "Nome1", "Sobrenome1", "NomeSobrenome1@email.com" );
-        Aluno aluno4 = new Aluno(2, 232455123, "Nome2", "Sobrenome2", "NomeSobrenome2@email.com" );
-        Aluno aluno5 = new Aluno("Nome3", "Sobrenome3");
-        Aluno aluno6 = new Aluno("Nome4", "Sobrenome4");
+        Aluno aluno1 = new Aluno(); //construtor padrão
+        Aluno aluno2 = new Aluno(); //construtor padrão
+        Aluno aluno3 = new Aluno(1, 324345642, "Diogo", "Fonseca", "diogo@email.com" ); //construtor parametrizado com todos os atributos
+        Aluno aluno4 = new Aluno(2, 232455123, "Tiago", "Petry", "tiago@email.com" ); //construtor parametrizado com todos os atributos
+        Aluno aluno5 = new Aluno("Rafael", "Castro"); //construtor parametrizado com apenas dois atributos
+        Aluno aluno6 = new Aluno("Emanuel", "Gonçalves"); //construtor parametrizado com apenas dois atributos
 
-//1.a)
+        //1.a)
         System.out.println(aluno1);
         System.out.println(aluno2);
         System.out.println(aluno3);
         System.out.println(aluno4);
         System.out.println(aluno5);
         System.out.println(aluno6);
+        //1.b)
+        aluno1.setId(10);
+        aluno1.setCpf(35123);
+        aluno1.setNome("Carlos");
+        aluno1.setSobrenome("Nobre");
+        aluno1.setEmail("carlos@email.com");
+
+        aluno2.setId(20);
+        aluno2.setCpf(354233);
+        aluno2.setNome("Joao");
+        aluno2.setSobrenome("Silva");
+        aluno2.setEmail("joao@email.com");
+
+        //Imprimir os atributos das instâncias modificadas usando getters
+        System.out.println("Aluno1 \nID: " + aluno1.getId() + "\n" + "CPF: " + aluno1.getCpf() +"\n"+ "NOME: " + aluno1.getNome()+"\n" + "SOBRENOME: " + aluno1.getSobrenome()+"\n" + "E-MAIL: " + aluno1.getEmail());
+        System.out.println("-----------------------------------");
+        System.out.println("Aluno2 \nID: " + aluno2.getId() + "\n" + "CPF: " + aluno2.getCpf() +"\n"+ "NOME: " + aluno2.getNome()+"\n" + "SOBRENOME: " + aluno2.getSobrenome()+"\n" + "E-MAIL: " + aluno2.getEmail());
+
     }
 }
